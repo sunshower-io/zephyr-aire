8b4cd07bbc04511b0d2d28fce335722006d728ea
Josiah Haswell <josiah.haswell@gmail.com>
2020-04-14 15:06:45 -0600

    Card is building

--- aire-card/src/main/java/io/sunshower/aire/AireCard.java
+++ aire-card/src/main/java/io/sunshower/aire/AireCard.java
@@ -7,7 +7,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 
 @Tag("aire-card")
 @JsModule("@sunshower-aire/aire-card/aire-card.js")
-@NpmPackage(value = "@sunshower-aire/aire-card", version = "^1.0.0")
+//@NpmPackage(value = "@sunshower-aire/aire-card", version = "^1.0.0")
 /*
  If you wish to include your own JS modules in the add-on jar, add the module
  files to './src/main/resources/META-INF/resources/frontend' and insert an

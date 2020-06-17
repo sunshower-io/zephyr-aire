a03e03c766c9199d5332348d4063cd0122e6794f
Josiah Haswell <josiah.haswell@gmail.com>
2020-06-16 19:35:11 -0600

    Adding splash

--- aire-installers/aire-windows-installer/src/main/resources/scripts/launch.cmd
+++ aire-installers/aire-windows-installer/src/main/resources/scripts/launch.cmd
@@ -4,6 +4,6 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
     SETLOCAL
     set "jre=%~dp0jre\bin\java.exe"
     set "jar=%~dp0installer\installer-win.jar"
-    CALL %jre% -jar %jar%
+    CALL %jre% -splash:assets/splash.png -jar %jar%
     EXIT /B %ERRORLEVEL%
 exit
